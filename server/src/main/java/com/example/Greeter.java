a16ed959c5bbe1c4389f2a605b1cd1418996330b
pgomes <pgomes@eurohelp.es>
2019-03-15 16:14:57 +0000

    alteracoes do checkstyle

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,9 +11,14 @@ public class Greeter {
   public Greeter() {
 
   }
+  
+  /**
+   * 
+   * @param someone name of person
+   * @return greeting
+   */
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
